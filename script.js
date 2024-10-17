1ec2220609b9af54350d7eb7ecf12a4258cb3d8c
eider98 <eideraranzabal@gmail.com>
2024-10-17 18:37:02 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,30 @@
+'use strict';
+const output = document.getElementById("output");
+const numberInput = document.getElementById("number");
+const button = document.getElementById("convert-btn");
+
+
+
+button.addEventListener('click',()=>{
+    const num = numberInput.value
+    if (!String(num)){
+        output.innerText = 'Please enter a valid number';
+    }
+    else if (num <=0) {
+        output.innerText = 'Please enter a number greater than or equal to 1';
+    }
+    else if (num>3999){
+        output.innerText = "Please enter a number less than or equal to 3999";
+    }
+    else{
+        romanConverter(num,);
+    }
+} )
+
+
+function romanConverter(num){
+    const romanObj ={'M':1000,'CM':900,'D':500,'CD':400,'C':100,'XC':90,'L':50,'XL':40,'X':10,'IX':9,'V':5,'IV':4,'I':1};
+    for (var key in romanObj){
+        
+    }
+}
